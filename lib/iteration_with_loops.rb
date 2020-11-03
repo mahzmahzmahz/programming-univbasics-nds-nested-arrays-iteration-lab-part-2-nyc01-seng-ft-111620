c02a47db6a5f115976d1b9ea9f1867ab53a8f738
Alex Marz <alexhmarz@gmail.com>
2020-11-03 22:23:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,10 @@ new_results = []
 row_index = 0
   while row.index < src.count do
     element_index = 0
+    while element_index < src[row_index].count do 
+      src[row_index][element_index].sort 
+      if 
+    
   end
 
 end
