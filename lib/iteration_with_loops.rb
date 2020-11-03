62287e6771cb73e407120ceb753bd25c05c47bf5
Alex Marz <alexhmarz@gmail.com>
2020-11-03 22:31:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,10 +7,8 @@ row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do 
-      
-    end
     element_index += 1 
   end
-  new_results << small_number
+  #new_results << small_number
   row_index += 1 
 end
